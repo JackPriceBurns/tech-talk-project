16d63e0f7796fbcc27bf13654065509638b21ba2
Jack Price-Burns <jack.price-burns@itech.media>
2021-02-05 14:02:05 +0000

    WIP on stuff

---
+++ app/Events/PhotoUploaded.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PhotoUploaded implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var string
+     */
+    public string $message;
+
+    /**
+     * PhotoUploaded constructor.
+     *
+     * @param string $message
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function broadcastOn(): array
+    {
+        return ['main-channel'];
+    }
+
+    /**
+     * @return string
+     */
+    public function broadcastAs(): string
+    {
+        return 'photo-uploaded';
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,15 +14,24 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        Schema::create(
+            'users',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('password');
+                $table->timestamps();
+            }
+        );
+
+        User::factory()->create(
+            [
+                'name' => 'Jack Price-Burns',
+                'email' => 'jackpriceburns@outlook.com',
+                'password' => password_hash('Admin1234!', PASSWORD_DEFAULT)
+            ]
+        );
     }
 
     /**
