49350c2c42bf2c38be177dcaa24d90b8b99ae7c8
Jack Price-Burns <jack.price-burns@itech.media>
2021-02-08 23:12:25 +0000

    Update grid again

--- resources/js/components/Photos.vue
+++ resources/js/components/Photos.vue
@@ -2,8 +2,14 @@
     <div class="p-6">
         <h1 class="text-3xl mb-4">Uploaded Photos</h1>
 
-        <div class="grid grid-cols-3 gap-4">
-            <div v-for="partition in partitionedPhotos">
+        <div class="grid-cols-3 gap-4 hidden md:grid">
+            <div v-for="partition in partitionedPhotos3">
+                <img :src="photo.url" class="w-full mt-4 object-cover rounded-lg" v-for="photo in partition"/>
+            </div>
+        </div>
+
+        <div class="grid-cols-2 gap-4 grid md:hidden">
+            <div v-for="partition in partitionedPhotos2">
                 <img :src="photo.url" class="w-full mt-4 object-cover rounded-lg" v-for="photo in partition"/>
             </div>
         </div>
@@ -26,25 +32,33 @@ export default {
     },
 
     computed: {
-        partitionedPhotos() {
+        partitionedPhotos3() {
+            return this.partition(this.photos, 3);
+        },
+
+        partitionedPhotos2() {
+            return this.partition(this.photos, 2);
+        },
+    },
+
+    methods: {
+        partition(photos, size) {
             let index = 0;
 
             return _.groupBy(
-                this.photos,
+                photos,
                 () => {
-                    if (index === 3) {
+                    if (index === size) {
                         index = 0;
                     }
 
                     index++;
 
-                    return (index - 1) % 3;
+                    return (index - 1) % size;
                 }
             );
         },
-    },
 
-    methods: {
         async loadPhotos() {
             try {
                 let {data: response} = await axios.get('/photos');
